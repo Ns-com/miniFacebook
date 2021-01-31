e13aad5f9f7ae0bdb90c883057466203d2c3b86d
acharyabibash <acharabibash@outlook.com>
2021-01-31 15:19:06 +0545

    Static files added

--- miniFacebok/urls.py
+++ miniFacebok/urls.py
@@ -18,7 +18,11 @@ from django.urls import path
 
 from .views import home_page
 
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home_page)
-]
+] +   static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
