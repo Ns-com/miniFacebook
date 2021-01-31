ca885e4e4a95baa263e514a21852d51c6b7ced76
acharyabibash <acharabibash@outlook.com>
2021-01-31 15:41:48 +0545

    Modified fb and index page

--- miniFacebok/views.py
+++ miniFacebok/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request,"messenger_page.html")
+    return render(request,"facebook_page.html")
+
+def index_page(request):
+    return render(request,"index.html")
