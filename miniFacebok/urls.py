57c25cda38f698579d74719687826b6457763a6a
acharyabibash <acharabibash@outlook.com>
2021-01-31 15:23:25 +0545

    just checkin

--- miniFacebok/urls.py
+++ miniFacebok/urls.py
@@ -25,4 +25,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home_page)
-] +   static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# fgfg
